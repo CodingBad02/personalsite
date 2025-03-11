ac53a87b75ca4c5caa97d9f9ba1b17d1c5e7194d
manjunathan-msd <manjunathan@madstreetden.com>
2025-03-11 19:29:00 +0530

    feature(adding support for gh pages)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/personalsite',       // Use if deploying under a repo subpath
+  assetPrefix: '/personalsite/',    // Use if deploying under a repo subpath
 };
 
 module.exports = nextConfig;
